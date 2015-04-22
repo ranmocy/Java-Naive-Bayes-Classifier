579e6c139293331d1879afd18f513eb9ba5d4bcc
Ranmocy Sheng <ranmocy@gmail.com>
2015-04-22 11:14:01 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/zsh
 
 set -e
 
+JAR_FILE=BayesClassifier.jar
+
 javac -cp '.' org/**/*.java
-jar cvfe BayesClassifier.jar org.classifier.example.RunnableExample org/**/*.class
+jar cvfe $JAR_FILE org.classifier.example.RunnableExample org/**/*.class
+java -jar $JAR_FILE
